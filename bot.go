4e86cc8bf6aa669b72a34872eeefea7818bb4dcc
Sirius <fhc023@gmail.com>
2018-04-22 12:18:20 +0800

    Retry when bot fail

--- bot.go
+++ bot.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"math"
+	"time"
+
 	"github.com/cirias/tgbot"
 	"github.com/pkg/errors"
 )
@@ -13,20 +17,28 @@ func Serve(bot *tgbot.Bot) error {
 	}
 
 	for {
-		updates, err := bot.GetUpdates(params)
-		if err != nil {
+		var updates []*tgbot.Update
+		err := willRetry(func() error {
+			var err error
+			updates, err = bot.GetUpdates(params)
 			return errors.Wrap(err, "could not get updates")
+		}, 4)
+		if err != nil {
+			return err
 		}
 
 		for _, u := range updates {
 			if u.Message.Text == "/ping" {
-				_, err := bot.SendMessage(&tgbot.SendMessageParams{
-					ChatId: u.Message.Chat.Id,
-					Text:   "pong",
-				})
+				err := willRetry(func() error {
+					_, err := bot.SendMessage(&tgbot.SendMessageParams{
+						ChatId: u.Message.Chat.Id,
+						Text:   "pong",
+					})
+					return errors.Wrap(err, "could not send message with tgbot")
+				}, 4)
 
 				if err != nil {
-					return errors.Wrap(err, "could not send message with tgbot")
+					return err
 				}
 			}
 		}
@@ -38,11 +50,27 @@ func Serve(bot *tgbot.Bot) error {
 }
 
 func SendMessage(bot *tgbot.Bot, chatId int64, m *Message) error {
-	_, err := bot.SendMessage(&tgbot.SendMessageParams{
-		ChatId:    chatId,
-		Text:      m.String(),
-		ParseMode: "markdown",
-	})
+	return willRetry(func() error {
+		_, err := bot.SendMessage(&tgbot.SendMessageParams{
+			ChatId:    chatId,
+			Text:      m.String(),
+			ParseMode: "markdown",
+		})
+		return errors.Wrap(err, "could not send message with tgbot")
+	}, 4)
+}
 
-	return errors.Wrap(err, "could not send message with tgbot")
+func willRetry(fn func() error, n int) error {
+	err := fn()
+	for i := 0; i < n; i += 1 {
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(math.Pow10(i)) * time.Millisecond * 100) // 0.1, 1, 10, 100
+
+		log.Printf("retry %d: %v\n", i, err)
+		err = fn()
+	}
+
+	return err
 }
