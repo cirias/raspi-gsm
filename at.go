17090c6327e39d2cb6b3ee7c942c75ed8db5aef9
Sirius <sirius@x250>
2020-12-20 15:31:29 +0800

    Refactor reading messages

--- at.go
+++ at.go
@@ -2,37 +2,38 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"io"
-	"log"
 	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 )
 
-type Event interface{}
-
-type ParseFunc func(*bufio.Scanner) (Event, error)
-
-type EventCMTI struct{}
-
-func ParseCMTI(scanner *bufio.Scanner) (Event, error) {
+func parseOKResponse(scanner *bufio.Scanner) (bool, error) {
 	line := scanner.Text()
-
-	if !strings.HasPrefix(line, "+CMTI: ") {
-		return nil, nil
+	if strings.Contains(line, " ERROR:") {
+		return false, errors.Errorf("response with error: %s", line)
+	}
+	if line != "OK" {
+		return false, nil
 	}
 
-	return &EventCMTI{}, nil
+	return true, nil
 }
 
-type EventCMGL struct {
+func parseCMTIResponse(scanner *bufio.Scanner) (bool, error) {
+	line := scanner.Text()
+	if !strings.HasPrefix(line, "+CMTI: ") {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+type CMGLResponse struct {
 	CMGL *CMGL
 	SMS  *SMS
 }
 
-func ParseCMGL(scanner *bufio.Scanner) (Event, error) {
+func parseCMGLResponse(scanner *bufio.Scanner) (*CMGLResponse, error) {
 	line := scanner.Text()
 
 	if !strings.HasPrefix(line, "+CMGL: ") {
@@ -54,124 +55,8 @@ func ParseCMGL(scanner *bufio.Scanner) (Event, error) {
 		return nil, errors.Wrapf(err, "could not decode SMS: %v", line)
 	}
 
-	return &EventCMGL{
+	return &CMGLResponse{
 		CMGL: cmgl,
 		SMS:  sms,
 	}, nil
-}
-
-type EventReader interface {
-	ReadEvent() (Event, error)
-}
-
-type RawEventReader struct {
-	scanner    *bufio.Scanner
-	parseFuncs []ParseFunc
-}
-
-func NewRawEventReader(r io.Reader) *RawEventReader {
-	return &RawEventReader{
-		scanner:    bufio.NewScanner(r),
-		parseFuncs: []ParseFunc{ParseCMTI, ParseCMGL},
-	}
-}
-
-func (r *RawEventReader) ReadEvent() (Event, error) {
-	// parsers := []ParseFunc{parseCMTI, parseCMGL}
-	for r.scanner.Scan() {
-		log.Println("line:", r.scanner.Text())
-		for _, parse := range r.parseFuncs {
-			event, err := parse(r.scanner)
-			if err != nil {
-				return nil, err
-			}
-			if event != nil {
-				return event, nil
-			}
-		}
-	}
-
-	return nil, errors.Wrapf(r.scanner.Err(), "could not scan")
-}
-
-type ConcatedMessageReader struct {
-	reader EventReader
-	lookup map[byte][]*SMS
-}
-
-func NewConcatedMessageReader(r EventReader) *ConcatedMessageReader {
-	return &ConcatedMessageReader{
-		reader: r,
-		lookup: make(map[byte][]*SMS),
-	}
-}
-
-type EventMessage struct {
-	From    string
-	Date    time.Time
-	Content string
-}
-
-func (m *EventMessage) String() string {
-	return fmt.Sprintf("*%s*\n*%s*\n%s", m.From, m.Date.Format(time.RFC3339), m.Content)
-}
-
-func (r *ConcatedMessageReader) ReadEvent() (Event, error) {
-	for {
-		event, err := r.reader.ReadEvent()
-		if err != nil {
-			return nil, err
-		}
-
-		e, ok := event.(*EventCMGL)
-		if !ok {
-			return event, nil
-		}
-
-		if e.CMGL.MessageStatus != ReceivedUnread {
-			continue
-		}
-
-		udhc := e.SMS.Tpdu.UDHConcatenated()
-
-		if udhc == nil {
-			return &EventMessage{
-				From:    e.SMS.Tpdu.OA,
-				Date:    e.SMS.Tpdu.SCTS,
-				Content: e.SMS.Tpdu.UD,
-			}, nil
-		}
-
-		ms, ok := r.lookup[udhc.Reference]
-		if !ok {
-			ms = make([]*SMS, udhc.Total)
-			r.lookup[udhc.Reference] = ms
-		}
-		ms[udhc.Index-1] = e.SMS
-
-		content := concatSMSContent(ms)
-		if content == "" {
-			continue
-		}
-
-		delete(r.lookup, udhc.Reference)
-		return &EventMessage{
-			From:    e.SMS.Tpdu.OA,
-			Date:    e.SMS.Tpdu.SCTS,
-			Content: content,
-		}, nil
-	}
-}
-
-func concatSMSContent(ms []*SMS) string {
-	content := ""
-	for _, m := range ms {
-		if m == nil {
-			return ""
-		}
-
-		content += m.Tpdu.UD
-	}
-
-	return content
 }
--- main.go
+++ main.go
@@ -2,10 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/cirias/tgbot"
@@ -39,61 +41,225 @@ func main() {
 		log.Fatalf("could not open port: %s", err)
 	}
 
-	reader, err := newMessageReader(port)
-	if err != nil {
-		log.Fatalln("could not new message reader:", err)
+	scanner := bufio.NewScanner(port)
+	if _, err := sendCommand(port, scanner, SetPDUMode); err != nil {
+		log.Fatalln(err)
+	}
+	if _, err := sendCommand(port, scanner, EnableErrorCode); err != nil {
+		log.Fatalln(err)
 	}
 
-	for {
-		m, err := readMessage(port, reader)
+	mc := &MessageConcatenator{lookup: make(map[byte]*concatenatorLookupRecord)}
+
+	if err := readSendDeleteMessages(bot, *chatId, port, scanner, mc); err != nil {
+		log.Fatalln(err)
+	}
+
+	for scanner.Scan() {
+		isCMTI, err := parseCMTIResponse(scanner)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln("could not parse CMTI response", err)
+		}
+		if !isCMTI {
+			continue
 		}
 
-		log.Printf("sending message:\n%s\n", m)
-		if err := SendMessage(bot, *chatId, m); err != nil {
+		if err := readSendDeleteMessages(bot, *chatId, port, scanner, mc); err != nil {
 			log.Fatalln(err)
 		}
 	}
 }
 
-func newMessageReader(port io.ReadWriter) (EventReader, error) {
-	if _, err := port.Write(SetPDUMode); err != nil {
-		return nil, fmt.Errorf("could not write to serial:", err)
+func readSendDeleteMessages(bot *tgbot.Bot, chatId int64, w io.Writer, scanner *bufio.Scanner, mc *MessageConcatenator) error {
+	var resps []*CMGLResponse
+	hasNewMessage := true
+	var err error
+	for hasNewMessage {
+		resps, hasNewMessage, err = listUnreadSMS(w, scanner)
+		if err != nil {
+			return errors.Wrapf(err, "could not list unread sms")
+		}
+
+		messages := mc.ListMessages(resps)
+		for _, message := range messages {
+			log.Printf("sending message:\n%s\n", message)
+			if err := SendMessage(bot, chatId, message); err != nil {
+				return errors.Wrapf(err, "could not send message")
+			}
+		}
+
+		hasNew, err := sendCommand(w, scanner, DeleteReadSMS)
+		if err != nil {
+			return errors.Wrapf(err, "could not send command")
+		}
+		hasNewMessage = hasNewMessage || hasNew
 	}
-
-	time.Sleep(100 * time.Millisecond)
-
-	if _, err := port.Write(EnableErrorCode); err != nil {
-		return nil, fmt.Errorf("could not write to serial:", err)
-	}
-
-	rawReader := NewRawEventReader(port)
-	return NewConcatedMessageReader(rawReader), nil
+	return nil
 }
 
-func readMessage(port io.Writer, r EventReader) (fmt.Stringer, error) {
-	for {
-		event, err := r.ReadEvent()
-		if err != nil {
-			return nil, errors.Wrap(err, "could not read event")
+type MessageConcatenator struct {
+	lookup map[byte]*concatenatorLookupRecord
+}
+
+type concatenatorLookupRecord struct {
+	timestamp time.Time
+	smses     []*SMS
+}
+
+type Message struct {
+	From    string
+	Date    time.Time
+	Content string
+}
+
+func (m *Message) String() string {
+	return fmt.Sprintf("*%s*\n*%s*\n%s", m.From, m.Date.Format(time.RFC3339), m.Content)
+}
+
+func (mc *MessageConcatenator) ListMessages(resps []*CMGLResponse) []*Message {
+	now := time.Now()
+
+	messages := make([]*Message, 0, len(resps))
+	for _, resp := range resps {
+		if resp.CMGL.MessageStatus != ReceivedUnread {
+			continue
 		}
 
-		switch event.(type) {
-		case *EventMessage:
-			return event.(*EventMessage), nil
-		case *EventCMTI:
-			if _, err := port.Write(DeleteReadSMS); err != nil {
-				return nil, errors.Wrap(err, "could not write to serial port")
-			}
+		udhc := resp.SMS.Tpdu.UDHConcatenated()
 
-			// wait or will get error 604: can not allocate control socket
-			// it's just take lots of time for deleting
-			time.Sleep(500 * time.Millisecond)
+		if udhc == nil {
+			messages = append(messages, &Message{
+				From:    resp.SMS.Tpdu.OA,
+				Date:    resp.SMS.Tpdu.SCTS,
+				Content: resp.SMS.Tpdu.UD,
+			})
+			continue
+		}
 
-			if _, err := port.Write(ListUnreadSMS); err != nil {
-				return nil, errors.Wrap(err, "could not write to serial port")
+		record, ok := mc.lookup[udhc.Reference]
+		if !ok {
+			record = &concatenatorLookupRecord{
+				timestamp: now,
+				smses:     make([]*SMS, udhc.Total),
 			}
+			mc.lookup[udhc.Reference] = record
+		}
+		record.smses[udhc.Index-1] = resp.SMS
+
+		content, end := concatSMSContent(record.smses)
+		if !end {
+			continue
+		}
+
+		messages = append(messages, &Message{
+			From:    resp.SMS.Tpdu.OA,
+			Date:    resp.SMS.Tpdu.SCTS,
+			Content: content,
+		})
+		delete(mc.lookup, udhc.Reference)
+	}
+
+	// append timeouted messages
+	for key, record := range mc.lookup {
+		if record == nil {
+			delete(mc.lookup, key)
+			continue
+		}
+		if len(record.smses) < 1 {
+			delete(mc.lookup, key)
+			continue
+		}
+		sms := record.smses[0]
+		if now.Sub(record.timestamp) < time.Minute {
+			content, _ := concatSMSContent(record.smses)
+			messages = append(messages, &Message{
+				From:    sms.Tpdu.OA,
+				Date:    sms.Tpdu.SCTS,
+				Content: content,
+			})
+			delete(mc.lookup, key)
+			continue
 		}
 	}
+
+	return messages
+}
+
+func concatSMSContent(ms []*SMS) (content string, end bool) {
+	for _, m := range ms {
+		if m == nil {
+			return content, false
+		}
+
+		content += m.Tpdu.UD
+	}
+
+	return content, true
+}
+
+func listUnreadSMS(w io.Writer, scanner *bufio.Scanner) (resps []*CMGLResponse, hasNewMessage bool, err error) {
+	if _, err := w.Write(ListUnreadSMS); err != nil {
+		return nil, hasNewMessage, errors.Wrapf(err, "could not write command: %s", strings.TrimSpace(string(ListUnreadSMS)))
+	}
+
+	resps = make([]*CMGLResponse, 0)
+	for scanner.Scan() {
+		log.Println("read line:", scanner.Text())
+
+		isOK, err := parseOKResponse(scanner)
+		if err != nil {
+			return nil, hasNewMessage, errors.Wrapf(err, "could not parse OK response")
+		}
+		if isOK {
+			return resps, hasNewMessage, nil
+		}
+
+		isCMTI, err := parseCMTIResponse(scanner)
+		if err != nil {
+			return nil, hasNewMessage, errors.Wrapf(err, "could not parse CMTI response")
+		}
+		if isCMTI {
+			hasNewMessage = true
+			continue
+		}
+
+		resp, err := parseCMGLResponse(scanner)
+		if err != nil {
+			return nil, hasNewMessage, errors.Wrapf(err, "could not parse CMGL response")
+		}
+		if resp != nil {
+			resps = append(resps, resp)
+		}
+	}
+
+	return nil, hasNewMessage, errors.Wrapf(err, "scanner ends unexpectedly")
+}
+
+func sendCommand(w io.Writer, scanner *bufio.Scanner, cmd []byte) (hasNewMessage bool, err error) {
+	if _, err := w.Write(cmd); err != nil {
+		return hasNewMessage, errors.Wrapf(err, "could not write command: %s", strings.TrimSpace(string(cmd)))
+	}
+
+	for scanner.Scan() {
+		log.Println("read line:", scanner.Text())
+
+		isOK, err := parseOKResponse(scanner)
+		if err != nil {
+			return hasNewMessage, errors.Wrapf(err, "could not parse OK response")
+		}
+		if isOK {
+			return hasNewMessage, nil
+		}
+
+		isCMTI, err := parseCMTIResponse(scanner)
+		if err != nil {
+			return hasNewMessage, errors.Wrapf(err, "could not parse CMTI response")
+		}
+		if isCMTI {
+			hasNewMessage = true
+			continue
+		}
+	}
+
+	return hasNewMessage, errors.Wrapf(err, "scanner ends unexpectedly")
 }
